50b909927c1f7d7f1e51354c749005dceac37dca
Jacques Comeaux <jacquesrcomeaux@protonmail.com>
2024-01-26 01:13:00 -0600

    Add parser for 3-bit register

--- assembler/register.s
+++ assembler/register.s
@@ -1,4 +1,4 @@
-register:
+register4:
   PUSH {LR}
 10:
   BL get_char
@@ -36,4 +36,27 @@ register:
   POP {PC}
 60:
   MOVS R4, 1
+  POP {PC}
+
+register3:
+  PUSH {LR}
+10:
+  BL get_char
+  MOVS R1, 'R
+  CMPS R0, R1
+  BNE 10b
+20:
+  BL get_char
+  MOVS R1, '0
+  CMPS R0, R1
+  BLO 20b
+  MOVS R1, '7
+  CMPS R0, R1
+  BHI 20b
+30:
+  SUBS R0, '0
+  MOVS R4, 0
+  ORRS R4, R0
+  BL get_char
+  BNE 40b
   POP {PC}
